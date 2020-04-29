2d70ae55f3e57ac3a0f45b6f168d2b0ba11b6a59
root <root@localhost.localdomain>
2020-04-29 07:50:00 +0530

    Updates

---
+++ precache-manifest.785e1a4fad1f35a28a6b9b9f4b6d6c89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a648d8e4f795d2f69ea8579efca30349",
+    "url": "/umang_portfolio/index.html"
+  },
+  {
+    "revision": "1c0a6819ca5454ddfc62",
+    "url": "/umang_portfolio/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "d1df5d3ed3751fbcc983",
+    "url": "/umang_portfolio/static/css/main.334ffd06.chunk.css"
+  },
+  {
+    "revision": "1c0a6819ca5454ddfc62",
+    "url": "/umang_portfolio/static/js/2.a345a907.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/umang_portfolio/static/js/2.a345a907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1df5d3ed3751fbcc983",
+    "url": "/umang_portfolio/static/js/main.4a16705f.chunk.js"
+  },
+  {
+    "revision": "c24ac6361f700c777f39",
+    "url": "/umang_portfolio/static/js/runtime-main.b999b785.js"
+  },
+  {
+    "revision": "ff844662d2149158019cdde1e6b803a5",
+    "url": "/umang_portfolio/static/media/my.ff844662.png"
+  }
+]);
